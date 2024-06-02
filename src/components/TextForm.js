ebb87d8d58007ef66fc3c25d6816e68a71d0ed75
maxus_red <incineroaraditya@gmail.com>
2024-06-02 19:53:50 +0530

    preview logic added

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,38 +1,78 @@
 import React, { useState } from "react";
 
 export default function TextForm(props) {
-  const [text, setText] = useState("Enter your text here: ");
+  const [text, setText] = useState("");
+  const [preText, setPretext] = useState("");
 
-  const handleText = (event)=>{
-    setText(event.target.value)
-  }
+  const handleText = (event) => {
+    let newtext = event.target.value;
+    setPretext(newtext);
+    setText(newtext);
+  };
 
-  const handleClickUpper = ()=>{
+  const handleClickUpper = () => {
+    let newText = text;
+    setText(newText.toUpperCase());
+  };
+  const handleClickLower = () => {
+    let newText = text;
+    setText(newText.toLowerCase());
+  };
+  const handleClickDollar = () => {
     let newText = text
-    setText(newText.toUpperCase())
-  }
-  const handleClickLower = ()=>{
-    let newText = text
-    setText(newText.toLowerCase())
-  }
-  const handleClickDollar = ()=>{
-    let newText = text.split("").map((i)=> i+="$").join("")
-    setText(newText)
-  }
+      .split("")
+      .map((i) => (i += "$"))
+      .join("");
+    setText(newText);
+  };
+
+  const handleClickConfirm = () => {
+    setPretext(text)
+  };
 
   return (
     <>
-      <h3>{props.heading}</h3>
+      <div className="container">
+        <h3>{props.heading}</h3>
+        <div className="form-group">
+          <textarea
+            className="form-control"
+            id="myTextarea"
+            rows="8"
+            value={preText}
+            onChange={handleText}
+          ></textarea>
+        </div>
+        <button className="btn btn-primary m-2" onClick={handleClickUpper}>
+          Upper Case
+        </button>
 
-      <div className="form-group">
-        <label htmlFor="myTextarea">Form:</label>
-        <textarea className="form-control" id="myTextarea" rows="8" value={text} onChange={handleText}></textarea>
+        <button className="btn btn-primary m-2" onClick={handleClickLower}>
+          Lower Case
+        </button>
+
+        <button
+          className="btn btn-primary m-2"
+          onClick={handleClickDollar}
+          disabled="true"
+        >
+          Dollarise
+        </button>
       </div>
-      <button className="btn btn-primary m-2" onClick={handleClickUpper}>Upper Case</button>
-
-      <button className="btn btn-primary m-2" onClick={handleClickLower}>Lower Case</button>
-
-      <button className="btn btn-primary m-2" onClick={handleClickDollar}>Dollarise</button>
+      <div className="container my-3">
+        <h3>Your Summary:</h3>
+        <p>
+          {text !== "" ? text.split(" ").length : 0} words and {text.length}{" "}
+          characters <br></br> {text !== "" ? (text.split(" ").length / 150).toFixed(2) : 0} minutes
+          read
+        </p>
+       
+        <h3>Preview:</h3>
+        <p>{text}</p>
+        <button className="btn btn-primary m-2" onClick={handleClickConfirm}>
+          Confirm
+        </button>
+      </div>
     </>
   );
 }
